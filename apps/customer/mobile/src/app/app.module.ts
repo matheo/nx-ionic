67933a808ed984fb6113c10316cba5948d22d0e3
Mateo Tibaquirá <nestormateo@gmail.com>
2019-09-25 10:38:44 -0500

    build: ng add @ionic/angular --project customer-mobile

--- apps/customer/mobile/src/app/app.module.ts
+++ apps/customer/mobile/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
