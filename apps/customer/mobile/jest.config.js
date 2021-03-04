c472c3fcc1e19bcb83f09083e56408b6a23d1a18
Mateo Tibaquira <nestormateo@gmail.com>
2021-03-04 18:07:59 -0500

    build: ng update @nrwl/workspace

--- apps/customer/mobile/jest.config.js
+++ apps/customer/mobile/jest.config.js
@@ -1,9 +1,20 @@
 module.exports = {
-  name: 'customer-mobile',
-  preset: '../../../jest.config.js',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/apps/customer/mobile',
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
+    'jest-preset-angular/HTMLCommentSerializer.js',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  displayName: 'customer-mobile',
 };
